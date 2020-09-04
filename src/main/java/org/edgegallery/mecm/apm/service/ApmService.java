f7bcf3f2d5bfe7012b81842513b7768706a04b86
root1 <vidyashree.rama@huawei.com>
2020-09-04 15:05:12 +0530

    Moving rest template to apm application

--- src/main/java/org/edgegallery/mecm/apm/service/ApmService.java
+++ src/main/java/org/edgegallery/mecm/apm/service/ApmService.java
@@ -59,7 +59,7 @@ public class ApmService {
     private String inventoryPort;
 
     @Autowired
-    private RestClientService restClientService;
+    private RestTemplate restTemplate;
 
     /**
      * Downloads app package csar from app store and stores it locally.
@@ -70,10 +70,8 @@ public class ApmService {
      */
     public String downloadAppPackage(String appPkgPath, String packageId, String tenantId) {
         ResponseEntity<InputStreamResource> response;
-        // Get Rest Template
-        RestTemplate template = restClientService.getRestTemplate();
         try {
-            response = template.getForEntity(appPkgPath, InputStreamResource.class);
+            response = restTemplate.getForEntity(appPkgPath, InputStreamResource.class);
         } catch (ResourceAccessException ex) {
             LOGGER.error(Constants.FAILED_TO_CONNECT_APPSTORE);
             throw new ApmException(Constants.FAILED_TO_CONNECT_APPSTORE);
@@ -111,10 +109,9 @@ public class ApmService {
                 .append("/mechosts/").append(hostIp).toString();
 
         ResponseEntity<String> response;
-        // Get Rest Template
-        RestTemplate template = restClientService.getRestTemplate();
+
         try {
-            response = template.getForEntity(url, String.class);
+            response = restTemplate.getForEntity(url, String.class);
         } catch (ResourceAccessException ex) {
             LOGGER.error(Constants.FAILED_TO_CONNECT_INVENTORY, ex.getMessage());
             throw new ApmException(Constants.FAILED_TO_CONNECT_INVENTORY);

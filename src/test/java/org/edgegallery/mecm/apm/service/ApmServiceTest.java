8600e169cef959eafaf3ce1e538e9729ad651290
root1 <vidyashree.rama@huawei.com>
2020-09-05 11:37:43 +0530

    testcases related to file operations

--- src/test/java/org/edgegallery/mecm/apm/service/ApmServiceTest.java
+++ src/test/java/org/edgegallery/mecm/apm/service/ApmServiceTest.java
@@ -16,24 +16,41 @@
 
 package org.edgegallery.mecm.apm.service;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import org.apache.commons.io.IOUtils;
+import org.edgegallery.mecm.apm.utils.ApmServiceHelper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApmServiceTest {
+
+    private static final String TENANT_ID = "18db0283-3c67-4042-a708-a8e4a10c6b32";
+    private static final String PACKAGE_ID = "f50358433cf8eb4719a62a49ed118c9b";
 
     @Autowired
     private ApmService apmService;
@@ -44,6 +61,24 @@ public class ApmServiceTest {
     private MockRestServiceServer mockServer;
 
     @Test
+    public void downloadAppPackageTest() throws FileNotFoundException {
+        File file = ResourceUtils.getFile("classpath:22406fba-fd5d-4f55-b3fa-89a45fee913a.csar");
+        InputStream ins = new BufferedInputStream(new FileInputStream(file.getPath()));
+        InputStreamResource inputStreamResource = new InputStreamResource(ins);
+        String url = "http://1.1.1.1:8099/mec/appstore/v1/apps/8ec923a8-9e30-4c94-a7ac-c92279488db2/packages"
+                + "/0fb274f2-213b-4a66-accc-ab218470caa3/action/download";
+
+        mockServer = MockRestServiceServer.createServer(restTemplate);
+        mockServer.expect(requestTo(url))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withSuccess(inputStreamResource, MediaType.APPLICATION_OCTET_STREAM));
+
+        InputStream response = apmService.downloadAppPackage(url, PACKAGE_ID, TENANT_ID);
+        assertNotNull(response);
+        mockServer.verify();
+    }
+
+    @Test
     public void getRepoInfoOfHostTest() {
         String url = "https://1.1.1.1:8080/inventory/v1/tenants/18db0283-3c67-4042-a708-a8e4a10c6b32/mechosts/1.1.1.1";
         String serviceResponseBody = "{'edgeRepoIp': '2.2.2.2', 'edgeRepoPort': 1234 }";
@@ -52,8 +87,25 @@ public class ApmServiceTest {
                 .andExpect(method(HttpMethod.GET))
                 .andRespond(withSuccess(serviceResponseBody, MediaType.APPLICATION_JSON));
 
-        String response = apmService.getRepoInfoOfHost("1.1.1.1",  "18db0283-3c67-4042-a708-a8e4a10c6b32");
+        String response = apmService.getRepoInfoOfHost("1.1.1.1",  TENANT_ID);
         assertEquals("2.2.2.2:1234", response);
         mockServer.verify();
+    }
+
+    @Test
+    public void testGetAppPackageFile() throws FileNotFoundException {
+        File file = ResourceUtils.getFile("classpath:22406fba-fd5d-4f55-b3fa-89a45fee913a.csar");
+        assertNotNull(apmService.getAppPackageFile(file.getPath()));
+    }
+
+    @Test
+    public void testDeleteFile() throws IOException {
+        File file = ResourceUtils.getFile("classpath:packages");
+        InputStream inputStream = IOUtils.toInputStream("mock data for test", "UTF-8");
+        String response = ApmServiceHelper.saveInputStreamToFile(inputStream, PACKAGE_ID, TENANT_ID, file.getPath());
+        assertNotNull(response);
+        File responseFile = new File(response);
+        assertTrue(responseFile.exists());
+        assertDoesNotThrow(() -> apmService.deleteAppPackageFile(response));
     }
 }

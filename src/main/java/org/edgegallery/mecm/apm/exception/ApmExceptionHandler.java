5c073010017d5ea35b04302eaa07c2a74b0d5a88
Pramod <pramod.kumar6@huawei.com>
2020-12-18 12:53:00 +0530

    Addressed the PR comments

--- src/main/java/org/edgegallery/mecm/apm/exception/ApmExceptionHandler.java
+++ src/main/java/org/edgegallery/mecm/apm/exception/ApmExceptionHandler.java
@@ -90,7 +90,7 @@ public class ApmExceptionHandler {
     }
 
     /**
-     * Returns error when Access is Denied.
+     * Returns error when access is denied.
      *
      * @param ex exception while processing request
      * @return response entity with error code and message

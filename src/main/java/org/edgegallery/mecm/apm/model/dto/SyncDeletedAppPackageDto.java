af7313576f685383934a09374bbbd02eeee21128
shashikanth.vh@huawei.com <shashikanth.vh@huawei.com>
2021-03-19 10:52:49 +0530

    package sync records models

---
+++ src/main/java/org/edgegallery/mecm/apm/model/dto/SyncDeletedAppPackageDto.java
@@ -0,0 +1,44 @@
+/*
+ *  Copyright 2021 Huawei Technologies Co., Ltd.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+ 
+package org.edgegallery.mecm.apm.model.dto;
+
+import java.util.LinkedList;
+import java.util.List;
+import javax.validation.Valid;
+import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.validation.annotation.Validated;
+
+/**
+ * App package info updated record synchronization request schema.
+ */
+@Validated
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class SyncDeletedAppPackageDto   extends SyncBaseDto {
+    @Size(max = 50, message = "App instance deleted records exceeds max limit 50")
+    private List<@Valid AppPackageDeletedDto> appPackageStaleRec = new LinkedList<>();
+
+    private List<@Valid AppPackageHostDeletedDto> appPackageHostStaleRec = new LinkedList<>();
+}

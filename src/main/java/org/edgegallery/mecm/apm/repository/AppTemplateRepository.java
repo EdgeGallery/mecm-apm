b71e962aeee134bb0a4205e7ed51408df27de34e
shashikanth.vh@huawei.com <shashikanth.vh@huawei.com>
2021-06-02 12:14:50 +0530

    application template repository

---
+++ src/main/java/org/edgegallery/mecm/apm/repository/AppTemplateRepository.java
@@ -0,0 +1,29 @@
+/*
+ *  Copyright 2021 Huawei Technologies Co., Ltd.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.edgegallery.mecm.apm.repository;
+
+import java.util.List;
+import org.edgegallery.mecm.apm.model.AppTemplate;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface AppTemplateRepository extends CrudRepository<AppTemplate, String> {
+
+    @Query(value = "SELECT * FROM appTemplate m WHERE m.tenant_id=:tenantId", nativeQuery = true)
+    List<AppTemplate> findByTenantId(@Param("tenantId") String tenantId);
+}
